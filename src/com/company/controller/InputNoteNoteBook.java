6379866ee27d26cab1b4e6cc1fa3d62f04c06d6b
Udjiin <udjinprofit@gmail.com>
2019-02-26 15:48:13 +0200

    Initial commit

---
+++ src/com/company/controller/InputNoteNoteBook.java
@@ -0,0 +1,22 @@
+package com.company.controller;
+
+import com.company.view.View;
+
+import java.util.Scanner;
+
+public class InputNoteNoteBook {
+    private View view;
+    private Scanner sc;
+
+    private String firstName;
+    private String login;
+
+    public InputNoteNoteBook(View view,Scanner sc){
+        this.view=view;
+        this.sc=sc;
+    }
+
+    public void inputNote(){
+
+    }
+}
---
+++ src/com/company/controller/UtilityController.java
@@ -0,0 +1,16 @@
+package com.company.controller;
+
+import com.company.view.View;
+
+
+import java.util.Scanner;
+
+public class UtilityController {
+    private Scanner scanner;
+    private View view;
+
+    public UtilityController(String message,String regex){
+        String res;
+
+    }
+}
